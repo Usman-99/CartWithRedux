7b03d7553e3d269ae95fb08961de824825542ea0
Usman-99 <usmanyousuf1137@gmail.com>
2024-08-08 20:47:49 +0500

    updating cart file

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { useSelector, useDispatch } from "react-redux";
-import { remove } from "../store/cartSlice";
+import { remove, increaseQuantity, decreaseQuantity } from "../store/cartSlice";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -10,6 +10,19 @@ const Cart = () => {
   const removefromCart = (id) => {
     dispatch(remove(id));
   };
+
+  const increaseQty = (id) => {
+    dispatch(increaseQuantity(id));
+  };
+
+  const decreaseQty = (id) => {
+    dispatch(decreaseQuantity(id));
+  };
+
+  const totalAmount = products.reduce(
+    (total, item) => total + item.product.price * item.quantity,
+    0
+  );
 
   const cardItems = products.map((item) => (
     <div className="col-md-12 mb-5" key={item.id}>
@@ -27,9 +40,24 @@ const Cart = () => {
           <Card.Text className="text-secondary">
             <b>Rs: {item.product.price}</b>
           </Card.Text>
+          <div className="d-flex justify-content-center align-items-center">
+            <Button
+              variant="outline-secondary"
+              onClick={() => decreaseQty(item.id)}
+            >
+              -
+            </Button>
+            <span className="mx-3">{item.quantity}</span>
+            <Button
+              variant="outline-secondary"
+              onClick={() => increaseQty(item.id)}
+            >
+              +
+            </Button>
+          </div>
         </Card.Body>
         <Card.Footer className="text-center bg-white">
-          <Button variant="warning" onClick={() => removefromCart(item.id)}>
+          <Button variant="danger" onClick={() => removefromCart(item.id)}>
             Remove Item
           </Button>
         </Card.Footer>
@@ -42,11 +70,14 @@ const Cart = () => {
       <h2 className="text-center">Your Cart</h2>
 
       {products.length === 0 ? (
-        <h4 className="text-center mt-5 text-primary">
-          No products added in cart
-        </h4>
+        <h5 className="text-center mt-5">No products added in cart</h5>
       ) : (
-        <div className="row">{cardItems}</div>
+        <>
+          <div className="row">{cardItems}</div>
+          <div className="text-center mt-2 mb-4">
+            <h4>Total Amount: Rs. {totalAmount.toFixed(2)}</h4>
+          </div>
+        </>
       )}
     </div>
   );
