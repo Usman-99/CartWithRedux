a3c3a18d966b8483f4bc565d4fadc48aee2098fc
Usman-99 <usmanyousuf1137@gmail.com>
2024-08-08 20:32:09 +0500

    updating cartslice

--- src/store/cartSlice.jsx
+++ src/store/cartSlice.jsx
@@ -6,14 +6,34 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     add(state, action) {
-      const item = { id: nanoid(), product: action.payload };
-      state.push(item);
+      const existingItem = state.find(
+        (item) => item.product.id === action.payload.id
+      );
+      if (existingItem) {
+        existingItem.quantity += 1; // Increase quantity if item already exists
+      } else {
+        const item = { id: nanoid(), product: action.payload, quantity: 1 };
+        state.push(item); // Add new item with quantity 1
+      }
     },
     remove(state, action) {
       return state.filter((item) => item.id !== action.payload); // Return the filtered state
     },
+    increaseQuantity(state, action) {
+      const item = state.find((item) => item.id === action.payload);
+      if (item) {
+        item.quantity += 1; // Increase the quantity of the item
+      }
+    },
+    decreaseQuantity(state, action) {
+      const item = state.find((item) => item.id === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1; // Decrease the quantity of the item
+      }
+    },
   },
 });
 
-export const { add, remove } = cartSlice.actions;
+export const { add, remove, increaseQuantity, decreaseQuantity } =
+  cartSlice.actions;
 export default cartSlice.reducer;
